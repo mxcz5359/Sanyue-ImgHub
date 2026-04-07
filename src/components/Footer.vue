95ccb4e640ccab44beb953461fc4f1a3508a181e
mxcz5359 <156735121+mxcz5359@users.noreply.github.com>
2026-04-07 13:20:19 +0800

    Update footer with new designer and styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,29 @@
 <template>
     <div class="page-footer" v-if="!disableFooter">
-        <p>© 2024-{{ thisYear }} Designed by <a class="footer-name" href="https://github.com/MarSeventh" target="_blank">SanyueQi</a> for You!
+        <p class="footer-text">
+            © 2024-{{ thisYear }} Designed by
+            <a class="footer-name" href="https://github.com/mxcz5359" target="_blank">YunMeng</a>
+            for You!
             <a :href="footerLink" target="_blank">
-                <font-awesome-icon icon="paper-plane" class="footer-link-icon"/>
+                <font-awesome-icon icon="paper-plane" class="footer-link-icon" />
             </a>
+            <br>
+            <span class="record-wrap">
+                备案号：
+                <a href="http://beian.miit.gov.cn/" rel="noopener" target="_blank" class="record-link">
+                    鲁ICP备2026009996号-2
+                </a>
+                <span class="police-record">
+                    <img
+                        alt="公安备案图标"
+                        src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png"
+                        class="record-icon"
+                    />
+                    <a href="https://beian.mps.gov.cn/#/query/webSearch?code=xxxxxxxxxxxxxxx" rel="noreferrer" target="_blank" class="record-link">
+                        鲁公网安备xxxxxxxxxxxxxxx号
+                    </a>
+                </span>
+            </span>
         </p>
     </div>
 </template>
@@ -37,12 +57,21 @@ export default {
     color: var(--page-footer-text-color);
     font-size: large;
     user-select: none;
+    text-align: center;
+    padding: 16px 12px;
 }
+
 @media (max-width: 768px) {
     .page-footer {
         font-size: small;
     }
 }
+
+.footer-text {
+    margin: 0;
+    line-height: 1.8;
+}
+
 .footer-name {
     color: var(--page-footer-name-color);
     font-weight: bold;
@@ -74,5 +103,34 @@ export default {
 
 .footer-link-icon:hover {
     transform: scale(1.2) rotate(-12deg);
+}
+
+.record-wrap {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 4px;
+}
+
+.police-record {
+    display: inline-flex;
+    align-items: center;
+    gap: 4px;
+}
+
+.record-link {
+    color: #666;
+    text-decoration: none;
+}
+
+.record-link:hover {
+    text-decoration: underline;
+}
+
+.record-icon {
+    width: 16px;
+    height: 16px;
+    vertical-align: middle;
 }
 </style>
